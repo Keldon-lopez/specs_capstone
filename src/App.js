be9ea6069069e03cdfecfd0ce30690db63efc19d
Keldon Lopez <keldon.lopz@gmail.com>
2023-08-15 20:16:51 -0600

    formatted some files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 // import logo from './logo.svg';
-import { Routes, Route, Navigate } from 'react-router-dom'
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MainDisplay from './components/MainDisplay';
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainDisplay from "./components/MainDisplay";
 
 function App() {
   return (
     <div className="App">
-      < Header />
+      <Header />
       <Routes>
-        <Route path='/' element={< MainDisplay />}/>
-        <Route path='*' element={<Navigate to='/'/>}/>
+        <Route path="/" element={<MainDisplay />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      < Footer />
-      
+      <Footer />
     </div>
   );
 }
